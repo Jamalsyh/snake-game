edfc6b1942baa5d33a15445d2e21865912260bb7
Jamalsyh <160244651+Jamalsyh@users.noreply.github.com>
2024-02-29 23:43:06 +0100

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -17,6 +17,13 @@ import pygame, sys, time, random
 # Hard      ->  40
 # Harder    ->  60
 # Impossible->  120
+
+# Difficulty settings
+# Easy      ->  10
+# Medium    ->  25
+# Hard      ->  40
+# Harder    ->  60
+# Impossible->  120
 difficulty = 25
 
 # Window size
